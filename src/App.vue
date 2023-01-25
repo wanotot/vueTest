e3ab398b5520d95b1fd95efe83f36163671f23cd
太田代和人 <wajin@ootashirokazutonoMacBook-Air.local>
2023-01-25 19:53:05 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Childcomp from './ChildComp.vue'
+
+export default {
+  components: {
+    Childcomp
+  },
+  data() {
+    return {
+      msg: 'from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp>Message: {{ msg }}</ChildComp>
+</template>
---
+++ src/ChildComp.vue
@@ -0,0 +1,5 @@
+
+
+<template>
+  <slot>Fallback content</slot>
+</template>
